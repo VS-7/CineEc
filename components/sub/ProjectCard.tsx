0155cf40513eb379f6b2bb2c58a24987c3600a7f
Vitor Sérgio <105945964+VS-7@users.noreply.github.com>
2024-02-01 17:31:44 -0300

    Update ProjectCard.tsx

--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -1,16 +1,14 @@
-import React from "react";
 import Image from "next/image";
+import React from "react";
 
 interface Props {
   src: string;
   title: string;
-  price: string;
   description: string;
-  platforms: string[];
-  subscribeLink: string;
+  link?: string; // Adicionando uma propriedade opcional para o link
 }
 
-const ProductCard = ({ src, title, price, description, platforms, subscribeLink }: Props) => {
+const ProjectCard = ({ src, title, description, link }: Props) => {
   return (
     <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]">
       <Image
@@ -23,28 +21,19 @@ const ProductCard = ({ src, title, price, description, platforms, subscribeLink 
 
       <div className="relative p-4">
         <h1 className="text-2xl font-semibold text-white">{title}</h1>
-        <p className="mt-2 text-gray-300">Valor: R$ {price}</p>
-        
-        <div className="mt-2 text-gray-300">
-          <p>Plataformas Inclusas:</p>
-          <ul>
-            {platforms.map((platform, index) => (
-              <li key={index}>{platform}</li>
-            ))}
-          </ul>
-        </div>
-
         <p className="mt-2 text-gray-300">{description}</p>
 
-        {/* Alterando o botão para "Assinar" */}
-        <a href={subscribeLink} target="_blank" rel="noopener noreferrer">
-          <button className="mt-4 bg-purple-500 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded">
-            Assinar
-          </button>
-        </a>
+        {/* Adicionando o botão com o link opcional */}
+        {link && (
+          <a href={link} target="_blank" rel="noopener noreferrer">
+            <button className="mt-4 bg-purple-500 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded">
+              Ver Projeto
+            </button>
+          </a>
+        )}
       </div>
     </div>
   );
 };
 
-export default ProductCard;
+export default ProjectCard;
