dad6177939d6a5bd829eaed5f949591ababac171
Vitor Sérgio <105945964+VS-7@users.noreply.github.com>
2024-02-01 18:00:19 -0300

    Update ProductCard.tsx

--- components/sub/ProductCard.tsx
+++ components/sub/ProductCard.tsx
@@ -5,10 +5,9 @@ interface Props {
   src: string;
   title: string;
   description: string;
-  onClick: () => void; // Adicionando propriedade de clique do botão
 }
 
-const ProductCard = ({ src, title, description, onClick }: Props) => {
+const ProjectCard = ({ src, title, description }: Props) => {
   return (
     <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]">
       <Image
@@ -22,17 +21,9 @@ const ProductCard = ({ src, title, description, onClick }: Props) => {
       <div className="relative p-4">
         <h1 className="text-2xl font-semibold text-white">{title}</h1>
         <p className="mt-2 text-gray-300">{description}</p>
-        
-        {/* Adicionando botão */}
-        <button
-          className="mt-4 bg-[#2A0E61] text-white py-2 px-4 rounded-md hover:bg-[#1D0749] focus:outline-none focus:ring focus:border-[#1D0749]"
-          onClick={onClick}
-        >
-          Comprar
-        </button>
       </div>
     </div>
   );
 };
 
-export default ProductCard;
+export default ProjectCard;
