c4c2e3cffec07f03feeb7cd1d131e50d308998f2
Vitor Sérgio <105945964+VS-7@users.noreply.github.com>
2024-02-01 17:11:58 -0300

    Update ProjectCard.tsx

--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -5,9 +5,10 @@ interface Props {
   src: string;
   title: string;
   description: string;
+  link?: string; // Adicionando uma propriedade opcional para o link
 }
 
-const ProjectCard = ({ src, title, description }: Props) => {
+const ProjectCard = ({ src, title, description, link }: Props) => {
   return (
     <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]">
       <Image
@@ -21,6 +22,15 @@ const ProjectCard = ({ src, title, description }: Props) => {
       <div className="relative p-4">
         <h1 className="text-2xl font-semibold text-white">{title}</h1>
         <p className="mt-2 text-gray-300">{description}</p>
+
+        {/* Adicionando o botão com o link opcional */}
+        {link && (
+          <a href={link} target="_blank" rel="noopener noreferrer">
+            <button className="mt-4 bg-purple-500 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded">
+              Ver Projeto
+            </button>
+          </a>
+        )}
       </div>
     </div>
   );
