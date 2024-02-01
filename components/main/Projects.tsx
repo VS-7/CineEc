fef0a20eb42ba48533c53ecae4a425ebb8d2586f
Vitor Sérgio <105945964+VS-7@users.noreply.github.com>
2024-02-01 17:34:55 -0300

    Update Projects.tsx

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -1,15 +1,17 @@
+import React from "react";
+import ProjectCard from "../sub/ProjectCard";
+
 const Projects = () => {
   return (
-    <div className="flex flex-col items-center justify-center py-20" id="projects">
-      <h1 className="text-[30px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
-        <center>Ao inscrever-se agora, você terá acesso garantido a:</center>
+    <div className="flex flex-col items-center justify-center py-10 md:py-20" id="projects">
+      <h1 className="text-2xl md:text-4xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-10">
+        My Projects
       </h1>
-      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-4 md:px-10">
+      <div className="flex flex-col md:flex-row gap-4 md:gap-10 px-4 md:px-10">
         <ProjectCard
           src="/NextWebsite.png"
-          title="Plano Mensal R$ 29,90"
-          description="Você terá acesso a: HBO Max(R$34,90) + Prime Video(R$14,90) + Disney Plus e Star+(R$ 55,90)"
-          link="https://seu-link-aqui.com" // Substitua pelo seu link real
+          title="Modern Next.js Portfolio"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
         />
         <ProjectCard
           src="/CardImage.png"
