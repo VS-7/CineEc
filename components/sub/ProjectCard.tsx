54f650a8683b13bb2e251756be5779fc08d1b51e
Vitor Sérgio <105945964+VS-7@users.noreply.github.com>
2024-02-01 20:18:19 -0300

    Update ProjectCard.tsx

--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -5,10 +5,10 @@ interface Props {
   src: string;
   title: string;
   description: string;
-  link: string;
+  link: string; // Adicionando a propriedade link ao tipo Props
 }
 
-const ProjectCard = ({ src, title, description }: Props) => {
+const ProjectCard = ({ src, title, description, link }: Props) => {
   return (
     <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]">
       <Image
@@ -23,7 +23,7 @@ const ProjectCard = ({ src, title, description }: Props) => {
         <h1 className="text-2xl font-semibold text-white">{title}</h1>
         <p className="mt-2 text-gray-300">{description}</p>
         
-       <a href={link} target="_blank" rel="noopener noreferrer">
+        <a href={link} target="_blank" rel="noopener noreferrer">
           <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline-blue active:bg-blue-800">
             Saiba mais
           </button>
