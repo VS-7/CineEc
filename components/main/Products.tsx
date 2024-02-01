a0865164a5c161361524f86ab0a2c8c96b719e7f
Vitor Sérgio <105945964+VS-7@users.noreply.github.com>
2024-02-01 17:54:29 -0300

    Update and rename Projects.tsx to Products.tsx

--- components/main/Products.tsx
+++ components/main/Products.tsx
@@ -1,31 +1,40 @@
 import React from "react";
-import ProjectCard from "../sub/ProjectCard";
+import ProductCard from "../sub/ProductCard"; // Renomeando para ProductCard
 
-const Projects = () => {
+const Products = () => {
+  const handleBuyClick = () => {
+    // Lógica para lidar com o clique no botão de compra
+    console.log("Botão Comprar clicado!");
+    // Adicione aqui a lógica de compra ou navegação para a página de compra
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center py-10 md:py-20" id="projects">
+    <div className="flex flex-col items-center justify-center py-10 md:py-20" id="products">
       <h1 className="text-2xl md:text-4xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-10">
         <center>Ao assinar agora, você terá acesso garantido a:</center>
       </h1>
       <div className="flex flex-col md:flex-row gap-4 md:gap-10 px-4 md:px-10">
-        <ProjectCard
+        <ProductCard
           src="/NextWebsite.png"
           title="Modern Next.js Portfolio"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          onClick={handleBuyClick} // Passando a função de clique
         />
-        <ProjectCard
+        <ProductCard
           src="/CardImage.png"
           title="Interactive Website Cards"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          onClick={handleBuyClick} // Passando a função de clique
         />
-        <ProjectCard
+        <ProductCard
           src="/SpaceWebsite.png"
           title="Space Themed Website"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          onClick={handleBuyClick} // Passando a função de clique
         />
       </div>
     </div>
   );
 };
 
-export default Projects;
+export default Products;
