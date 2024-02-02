8468b7b864d62ea004d6654834a2392bc931940c
Vitor Sérgio <105945964+VS-7@users.noreply.github.com>
2024-02-02 13:58:19 -0300

    Update ProjectCard.tsx

--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -32,11 +32,13 @@ const ProjectCard = ({ src, title, description, projectLink }: Props) => {
         <p className="mt-2 text-gray-300">{description}</p>
 
         {/* Botão para acessar o projeto */}
-        <a href={projectLink} target="_blank" rel="noopener noreferrer">
-          <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Economize 70% AGORA
-          </button>
-        </a>
+         <motion.a
+          variants={slideInFromLeft(1)}
+          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
+          href="#projects"
+        >
+          CLIQUE PARA ECONOMIZAR 70%
+        </motion.a>
       </div>
     </div>
   );
