daf0770386d9a6ef83b2528ab5d209a03fc447a0
Vitor Sérgio <105945964+VS-7@users.noreply.github.com>
2024-02-02 13:34:14 -0300

    Update ProjectCard.tsx

--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
 import React from "react";
+import { motion } from "framer-motion";
+import {
+  slideInFromLeft,
+  slideInFromRight,
+  slideInFromTop,
+} from "@/utils/motion";
 
 interface Props {
   src: string;
