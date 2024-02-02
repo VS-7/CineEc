c099f9d3af3fb797a8e119e23060af166b0c768e
Vitor Sérgio <105945964+VS-7@users.noreply.github.com>
2024-02-02 15:48:00 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,9 +22,8 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
       >
-        
-        <Navbar />
         <StarCanvas />
+        <Navbar />
         {children}
         <Footer />
       </body>
