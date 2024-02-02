786c400c65068fbee3f39e330e8e05c0646c38d6
Vitor Sérgio <105945964+VS-7@users.noreply.github.com>
2024-02-02 13:41:41 -0300

    Update ProjectCard.tsx

--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -1,43 +1,34 @@
-import Image from "next/image";
 import React from "react";
-import { motion } from "framer-motion";
-import { slideInFromLeft, slideInFromRight, slideInFromTop } from "@/utils/motion";
+import ProjectCard from "../sub/ProjectCard";
 
-interface Props {
-  src: string;
-  title: string;
-  description: string;
-  projectLink: string; // Adicionando a propriedade do link do projeto
-}
-
-const ProjectCard = ({ src, title, description, projectLink }: Props) => {
+const Projects = () => {
   return (
-    <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]">
-      <Image
-        src={src}
-        alt={title}
-        width={1000}
-        height={1000}
-        className="w-full object-contain"
-      />
-
-      <div className="relative p-4">
-        <h1 className="text-2xl font-semibold text-white">{title}</h1>
-        <p className="mt-2 text-gray-300">{description}</p>
-
-        {/* Botão para acessar o projeto */}
-        <motion.a
-          variants={slideInFromLeft(1)}
-          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
-          href={projectLink}  // Adicione o link do projeto ao href
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Economize 70% AGORA
-        </motion.a>
+    <div className="flex flex-col items-center justify-center py-10 md:py-20" id="projects">
+      <h1 className="text-2xl md:text-4xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-10">
+        <center>Ao assinar agora, você terá acesso garantido a:</center>
+      </h1>
+      <div className="flex flex-col md:flex-row gap-4 md:gap-10 px-4 md:px-10">
+        <ProjectCard
+          src="/Combo.png"
+          title="Combo Mensal - R$ 29,90"
+          description="HBO Max(R$ 34,90) + DisneyPlus + Star+ (R$ 55,90) + Prime Vídeo (R$14,90)"
+          projectLink="/link-para-o-seu-projeto1" // Substitua com o link real para o primeiro projeto
+        />
+        <ProjectCard
+          src="/CardImage.png"
+          title="Interactive Website Cards"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          projectLink="/link-para-o-seu-projeto2" // Substitua com o link real para o segundo projeto
+        />
+        <ProjectCard
+          src="/SpaceWebsite.png"
+          title="Space Themed Website"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          projectLink="/link-para-o-seu-projeto3" // Substitua com o link real para o terceiro projeto
+        />
       </div>
     </div>
   );
 };
 
-export default ProjectCard;
+export default Projects;
