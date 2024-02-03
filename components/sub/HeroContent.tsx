a1df7d981063feaf3ad50b7f5724798ccfd78fa2
Vitor Sérgio <105945964+VS-7@users.noreply.github.com>
2024-02-03 11:51:32 -0300

    Update HeroContent.tsx

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -62,7 +62,7 @@ const HeroContent = () => {
         className="w-full h-full flex justify-center items-center"
       >
         <Image
-          src="/mainIconsd.svg"
+          src="/mainIconsd.png"
           alt="work icons"
           height={650}
           width={650}
