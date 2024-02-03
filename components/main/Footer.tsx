e32f96d604929a52ca6ae7ba2f3d44cb2795bb16
Vitor Sérgio <105945964+VS-7@users.noreply.github.com>
2024-02-03 10:17:09 -0300

    Update Footer.tsx

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -21,7 +21,7 @@ const Footer = () => {
                         <RxInstagramLogo />
                     </p>
                 </div>
-                <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
+                <div className="min-w-[150px] h-auto flex flex-col items-center justify-start">
                     <div className="font-bold text-[16px]">Contato</div>
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
                       
