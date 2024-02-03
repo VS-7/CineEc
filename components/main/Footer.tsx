e46ee4d7a367db16d6b89ff8c28944f200b72d0f
Vitor Sérgio <105945964+VS-7@users.noreply.github.com>
2024-02-03 10:01:39 -0300

    Update Footer.tsx

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -24,7 +24,7 @@ const Footer = () => {
                     </p>
                 </div>
                 <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
-                    <div className="font-bold text-[16px]">Atendimento</div>
+                    <div className="font-bold text-[16px]">Contato</div>
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
                       
                         <span className="text-[15px] ml-[6px]">Whatsapp (33)999123315</span>    
