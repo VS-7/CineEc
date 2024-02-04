eddd302e41a162cad9bd6cac54130fc0a885301c
Vitor Sérgio <105945964+VS-7@users.noreply.github.com>
2024-02-04 07:44:47 -0300

    Update HeroContent.tsx

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -33,7 +33,7 @@ const HeroContent = () => {
           className="flex flex-col gap-6 mt-6 text-4xl font-bold text-white max-w-[600px] w-auto h-auto"
         >
           <span>
-            Obtenha R$ 100,00 em plataformas de stream
+            Obtenha R$ 100,00 em plataformas de streaming
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
               {" "}
               por apenas R$ 16,90.{" "}
@@ -46,7 +46,7 @@ const HeroContent = () => {
           variants={slideInFromLeft(0.8)}
           className="text-lg text-gray-400 my-5 max-w-[400px]"
         >
-          Obtenha acesso as melhores plataformas de stream oficiais de filmes e séries com 84% de desconto.
+          Obtenha acesso as melhores plataformas de streaming oficiais de filmes e séries com 84% de desconto.
         </motion.p>
         <motion.a
           variants={slideInFromLeft(1)}
