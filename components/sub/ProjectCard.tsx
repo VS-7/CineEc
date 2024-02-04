51e6548170cb1a2d63c7bfe44a1f8ca3dedac655
Vitor Sérgio <105945964+VS-7@users.noreply.github.com>
2024-02-04 07:59:23 -0300

    Update ProjectCard.tsx

--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -37,29 +37,7 @@ const ProjectCard = ({ src, title, description, projectLink }: Props) => {
       </div>
 
       {/* Segundo Card */}
-      <div className="flex-1 relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]">
-        <Image
-          src={src}
-          alt={title}
-          width={1000}
-          height={1000}
-          className="w-full object-contain"
-        />
-
-        <div className="relative p-4">
-          <h1 className="text-2xl font-semibold text-white">{title}</h1>
-          <p className="mt-2 text-gray-300">{description}</p>
-
-          <a
-            href={projectLink}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="block mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer"
-          >
-            ADQUIRIR AGORA
-          </a>
-        </div>
-      </div>
+     
     </div>
   );
 };
