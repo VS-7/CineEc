bcc692702e186d341a143bba511bf38e37b97613
Vitor Sérgio <105945964+VS-7@users.noreply.github.com>
2024-02-04 11:21:11 -0300

    Update ProjectCardDuplo.tsx

--- components/sub/ProjectCardDuplo.tsx
+++ components/sub/ProjectCardDuplo.tsx
@@ -46,7 +46,13 @@ const SecondProjectCard = () => {
   // Dados de exemplo para o segundo projeto
   const project2 = {
     src: "disneystar.png",
-    title: "R$ 9,90",
+    title: "R$ 11,90",
+    description: "Tenha acesso as plataformas(R$ 55,90)",
+    projectLink: "https://link-do-projeto-2.com",
+  };
+  const project3 = {
+    src: "canva.png",
+    title: "R$ 5,90",
     description: "Tenha acesso as plataformas(R$ 55,90)",
     projectLink: "https://link-do-projeto-2.com",
   };
@@ -63,6 +69,29 @@ const SecondProjectCard = () => {
 
       <div className="relative p-4">
         <h1 className="text-2xl font-semibold text-white">{project2.title}</h1>
+        <p className="mt-2 text-gray-300">{project2.description}</p>
+
+        <a
+          href={project2.projectLink}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="block mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer"
+        >
+          COMPRAR
+        </a>
+      </div>
+    </div>
+    <div className="flex-1 relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]">
+      <Image
+        src={project3.src}
+        alt={project3.title}
+        width={1000}
+        height={1000}
+        className="w-full object-contain"
+      />
+
+      <div className="relative p-4">
+        <h1 className="text-2xl font-semibold text-white">{project3.title}</h1>
         <p className="mt-2 text-gray-300">{project2.description}</p>
 
         <a
