e9e8c80585f37239dfe3a0ec82a04997ad864c2c
Vitor Sérgio <105945964+VS-7@users.noreply.github.com>
2024-02-04 08:04:19 -0300

    Update Projects.tsx

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import ProjectCard from "../sub/ProjectCard";
+import ProjectCardDuplo from "../sub/ProjectCardDuplo";
 
 const Projects = () => {
   return (
@@ -10,6 +11,12 @@ const Projects = () => {
         <center>Ao assinar agora, você terá acesso garantido a:</center>
       </h1>
       <div className="flex flex-col md:flex-row gap-4 md:gap-10 px-4 md:px-10">
+         <ProjectCardDuplo
+          src="/Combo.png"
+          title="Combo Plus Mensal - R$ 29,90"
+          description="Tenha acesso as plataformas HBO Max(R$ 34,90)  | DisneyPlus + Star+ (R$ 55,90)  | Prime Vídeo (R$14,90) "
+          projectLink="https://cine-economico.pay.yampi.com.br/r/K57J4VZNK1" // Substitua com o link real para o primeiro projeto
+        />
         <ProjectCard
           src="/Combo.png"
           title="Combo Plus Mensal - R$ 29,90"
